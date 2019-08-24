a445cdddea7db11fe2ce3000b2eda595bc557498
Michael Brandt <morbidfukk@gmail.com>
2019-08-25 00:27:47 +0200

    Updates

---
+++ precache-manifest.91eea15741801139c45f32fb6e076ea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7c490a6b761226265c2a16c883a3f7",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "94a703ef9a9c0d5806d8",
+    "url": "/MonstersRolodex/static/css/main.42cec95d.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/MonstersRolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "94a703ef9a9c0d5806d8",
+    "url": "/MonstersRolodex/static/js/main.e608167f.chunk.js"
+  },
+  {
+    "revision": "ed02362edc39d8d96650",
+    "url": "/MonstersRolodex/static/js/runtime~main.f46fcb36.js"
+  }
+]);
